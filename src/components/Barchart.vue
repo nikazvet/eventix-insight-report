25c04509bd3206d7bc532ca99a9276a152252b0b
Veronika P-CB02 Student 4173287 <v.tsvietkova@student.fontys.nl>
2022-11-15 15:00:17 +0100

    Load svg array and get an array of ages

---
+++ src/components/Barchart.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>Graph can go below</p>
+  <Bar
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :chart-id="chartId"
+    :width="400"
+    :height="400"
+  />
+</template>
+
+<script setup>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+defineProps({
+  chartData: Array,
+})
+var chartOptions = {responsive: true};
+var chartId = "ageChart";
+console.log("processed");
+</script>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from 'vue'
+import Papa from 'papaparse'
+import Barchart from './Barchart.vue'
+    var bigFile = $ref(null);
+    var ages = $ref([]);
+    var dataLoaded = $ref(false);
+
+    function previewFiles(e) 
+    {
+        bigFile = e.target.files[0];
+    }
+
+    function parse(){
+        // Stream big file in worker thread
+        Papa.parse(bigFile, {
+        worker: true,
+        dynamicTyping: true,
+        header: true,
+        /*transformHeader: function(header, index){
+            header = header.replaceAll(".", "-");
+            return header;
+        },*/
+
+        step: function(results) {
+            ages.push(results.data.order_metadata_age); 
+            //console.log(results.data.order_metadata_age);
+            //console.log("Row:", results.data);
+	    },
+        complete: function() {
+            console.log(ages.length);
+		console.log("All done!");
+        dataLoaded = true;
+	    }
+    })
+        
+    }
+</script>
+
+<template>
+    <input type="file" @change="event => previewFiles(event)"/>
+    <p>Text</p>
+    <button @click="parse">Parse</button>
+
+    <Barchart v-if="dataLoaded" :chart-data="ages"></Barchart>
+
+</template>
