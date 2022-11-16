1cb329ea3d5d54f48d802c989b80a42f5a09d397
Veronika P-CB02 Student 4173287 <v.tsvietkova@student.fontys.nl>
2022-11-16 13:33:16 +0100

    Version with non-dinamic colors

--- src/components/Barchart.vue
+++ src/components/Barchart.vue
@@ -1,15 +1,22 @@
 <template>
   <br/>
   <h1>Set settings for graph</h1>
+  From
   <input type="number" v-model="minAge"/>
+  To
   <input type="number" v-model="maxAge"/>
-  <input type="number" v-model="binWidth"/>
   <button @click="RenderGraph">Go!</button>
   <p>Graph can go below</p>
   <Bar v-if="showGraph"
     :chart-options="chartOptions"
-    :chart-data="binnedChartData"
-    :chart-id="chartId"
+    :chart-data="{
+        labels: binnedChartData.map(row => row.age),
+        datasets: [
+          {
+            label: 'Ages',
+            data: binnedChartData.map(row => row.count)
+          }
+        ]}"
     :width="400"
     :height="400"
   />
@@ -19,35 +26,57 @@
 import { Bar } from 'vue-chartjs'
 import { ref } from 'vue'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-defineProps({
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+function Colorize(opaque) 
+    {
+  return (ctx) => {
+    const v = ctx.parsed.y;
+    const c = v < -50 ? '#D60000'
+      : v < 20 ? '#F46300'
+      : v < 276 ? '#0358B6'
+      : '#44DE28';
+
+    return opaque ? c : c /*Utils.transparentize(c, 1 - Math.abs(v / 150))*/;
+  };
+}
+
+var binnedChartData = $ref(new Array());
+const minAge = $ref(6);
+const maxAge = $ref(100);
+var showGraph = $ref(false);
+var chartOptions = {
+  responsive: true, 
+  elements: {
+      bar: {
+        backgroundColor: Colorize(false),
+        borderWidth: 2
+      }
+    }};
+console.log("processed");
+const props = defineProps({
   chartData: Array,
 })
-var binnableData = ref(chartData);
-var minAge = $ref(6);
-var maxAge = $ref(100);
-var binWidth = $ref(1);
-var showGraph = $ref(false);
-var binnedChartData = $ref(binnableData);
-var chartOptions = {responsive: true};
-var chartId = "ageChart";
-console.log("processed");
-
 function RenderGraph(){
-  CreateBins(binnableData);
+  CreateBins();
   showGraph = true;
 }
 
-function CreateBins(chartData){
-  binnedChartData = [];
-  for (let index = 0; index < ((maxAge - minAge)/binWidth + 1); index++) {
-    binnedChartData.push({age: index + minAge, count: 0});
+function CreateBins(){
+  binnedChartData = new Array((maxAge - minAge) + 1);
+  for (let index = 0; index < ((maxAge - minAge) + 1); index++) {
+    binnedChartData[(index - minAge) + 1] = ({age: index + minAge, count: 0});
   }
-  chartData.forEach(element => {
+  props.chartData.forEach(element => {
     if(element != null){
-      if(element >= minAge || element < maxAge){
-        binnedChartData[element - minAge].count++;
+      if(element >= minAge && element < maxAge){
+        //console.log(element);
+        binnedChartData[(element - minAge)].count++;
       }
     }
+    
   });
+  console.log("Data binned");
 }
 </script>
